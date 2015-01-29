86cdca063f41835f7acf068df463f14eecd11e17
Remco <remco@dutchcoders.io>
2015-01-29 16:29:59 +0100

    fixed issue with not overriding of existing value of map, and added expect test

--- merge.go
+++ merge.go
@@ -53,8 +53,10 @@ func deepMerge(dst, src reflect.Value, visited map[uintptr]*visit, depth int) (e
 				if err = deepMerge(dstElement, srcElement, visited, depth+1); err != nil {
 					return
 				}
-			}
-			if !dstElement.IsValid() {
+				if !dstElement.IsValid() {
+					dst.SetMapIndex(key, srcElement)
+				}
+			default:
 				dst.SetMapIndex(key, srcElement)
 			}
 		}
