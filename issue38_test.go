a05b32b6c0121cd647602acd5c8c27ab606ebb6f
Dario Castañé <i@dario.im>
2017-10-09 19:54:33 +0200

    Fixing test

--- issue38_test.go
+++ issue38_test.go
@@ -21,8 +21,24 @@ func TestIssue38Merge(t *testing.T) {
 	if err := Merge(&dst, src); err != nil {
 		t.Errorf("Error while merging %s", err)
 	}
-	if dst.Created != src.Created {
-		t.Fatalf("Created not merged in properly: dst.Created(%v) != src.Created(%v)", dst.Created, src.Created)
+	if dst.Created == src.Created {
+		t.Fatalf("Created merged unexpectedly: dst.Created(%v) == src.Created(%v)", dst.Created, src.Created)
+	}
+}
+
+func TestIssue38MergeEmptyStruct(t *testing.T) {
+	dst := structWithoutTimePointer{
+	}
+
+	expected := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	src := structWithoutTimePointer{
+		expected,
+	}
+	if err := Merge(&dst, src); err != nil {
+		t.Errorf("Error while merging %s", err)
+	}
+	if dst.Created == src.Created {
+		t.Fatalf("Created merged unexpectedly: dst.Created(%v) == src.Created(%v)", dst.Created, src.Created)
 	}
 }
 
