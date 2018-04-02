baab95ca2b68c75a23b78c646ae5947b76355f73
Dario Castañé <i@dario.im>
2018-04-02 15:47:48 +0200

    Improved format for #66 test

--- issue66_test.go
+++ issue66_test.go
@@ -5,8 +5,7 @@ import (
 )
 
 type PrivateSliceTest66 struct {
-	PublicStrings []string
-
+	PublicStrings  []string
 	privateStrings []string
 }
 
@@ -15,19 +14,15 @@ func TestPrivateSlice(t *testing.T) {
 		PublicStrings:  []string{"one", "two", "three"},
 		privateStrings: []string{"four", "five"},
 	}
-
 	p2 := PrivateSliceTest66{
 		PublicStrings: []string{"six", "seven"},
 	}
-
 	if err := Merge(&p1, p2); err != nil {
 		t.Fatalf("Error during the merge: %v", err)
 	}
-
 	if len(p1.PublicStrings) != 5 {
 		t.Error("5 elements should be in 'PublicStrings' field")
 	}
-
 	if len(p1.privateStrings) != 2 {
 		t.Error("2 elements should be in 'privateStrings' field")
 	}
