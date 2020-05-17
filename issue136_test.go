ea5937aad2199936af9518660a86007bcc5761de
Dario Castañé <i@dario.im>
2020-05-17 14:58:37 +0200

    Issue #136 resolved

---
+++ issue136_test.go
@@ -0,0 +1,29 @@
+package mergo
+
+import (
+	"testing"
+)
+
+type embeddedTestA struct {
+	Name string
+	Age  uint8
+}
+
+type embeddedTestB struct {
+	embeddedTestA
+	Address string
+}
+
+func TestMergeEmbedded(t *testing.T) {
+	a := &embeddedTestA{
+		"Suwon", 16,
+	}
+
+	b := &embeddedTestB{}
+
+	err := Merge(&b.embeddedTestA, *a)
+
+	if b.Name != "Suwon" {
+		t.Fatalf("%v %v", b.Name, err)
+	}
+}
